997ecbe8da824d29f7e9e037efba9d77efc95251
kurniacf <kcf170202@gmail.com>
2021-11-24 12:53:17 +0700

    fix register user

--- auth/users/register.php
+++ auth/users/register.php
@@ -53,7 +53,7 @@ if (!empty($_POST['name_user']) && !empty($_POST['nrp_user']) && !empty($_POST['
     }
 } else {
     http_response_code(400);
-    set_response(false, "Dont Empty!!", "Fill in NRP and Password");
+    set_response(false, "Dont Empty!!", "Fill in NRP, Name, and Password");
 }
 
 function set_response($isSuccess, $message, $data)
